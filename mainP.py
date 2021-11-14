dfdf423fe3fefa34940e60740cf0751c588c15b5
Anahi <anahi21hdz@gmail.com>
2021-11-13 21:49:02 -0600

    las cosas son raras

--- mainP.py
+++ mainP.py
@@ -1,2 +1,7 @@
+import test1 as Entrevista
 "¨Programa principal para la entrevista de un nuevo empleado de trabajo"
 print("Cargar nuevo archivo")
+#llegada de una persona a la empresa para la entrevista de trabajo
+print("la empresa SabalodeXalapa suc. Teziutlan")
+print("Una persona se acerca a la ventanilla")
+#Secretaria1=Entrevista.Persona("Mi nombre es Daniel",)
